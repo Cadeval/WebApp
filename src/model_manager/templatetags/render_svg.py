d5fe71121634bfeabb33200fe9d4cadf0e67b730
Hallas, Mia <mia@meanderingmind.me>
2025-01-08 02:42:01 +0100

    Implement a text to svg renderer
    Signed-off-by: Hallas, Mia <mia@meanderingmind.me>

--- src/model_manager/templatetags/render_svg.py
+++ src/model_manager/templatetags/render_svg.py
@@ -10,11 +10,9 @@ from django.template.defaulttags import register
 @register.filter
 def render_svg(
         value,
-        width=100,
-        height=200,
+        font_size=3,
         color_start="red",
         color_end="blue",
-        font_size=60
 ):
     """
     Create an SVG string that renders the given value as text with a gradient.
@@ -29,23 +27,23 @@ def render_svg(
     """
     text_value = str(value)  # Convert integer/float to string if necessary
 
+    # width="{width}" height="{height}"
+
     # Define the SVG markup
     svg_template = f"""
 <svg version="1.1" baseProfile="full"
-     width="auto" height="auto"
      xmlns="http://www.w3.org/2000/svg">
   <defs>
-    <linearGradient id="textGradient" x1="0%" y1="0%" x2="100%" y2="0%">
-      <stop offset="0%" stop-color="{color_start}" />
-      <stop offset="100%" stop-color="{color_end}" />
-    </linearGradient>
+  <text-color>{color_start}</text-color>
   </defs>
 
   <text x="50%" y="50%"
+  
         text-anchor="middle"
         alignment-baseline="middle"
-        font-size="{font_size}"
-        fill="url(#textGradient)">
+        font-size="{font_size}em"
+        fill="#--primary-color"
+        text-color="{color_end}">
     {text_value}
   </text>
 </svg>
